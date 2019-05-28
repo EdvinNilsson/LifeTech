e3290d46c83fe559f0c92ff13a5849ca2d1590b8
Edvin Nilsson <edvinn.nilsson@gmail.com>
2019-05-28 15:52:34 +0200

    Add Arduino project.

--- MainServer/Program.cs
+++ MainServer/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace MainServer
+namespace WebServer
 {
     public class Program
     {
--- MainServer/Startup.cs
+++ MainServer/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace MainServer
+namespace WebServer
 {
     public class Startup
     {
