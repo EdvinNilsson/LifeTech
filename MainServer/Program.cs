28c93afe19e51e2ad8a0d227ca903ea5f0bd26af
RedE01 <e.sternvik@gmail.com>
2019-10-15 10:42:01 +0200

    Refactor webserver to new file, add simple threading example

--- MainServer/Program.cs
+++ MainServer/Program.cs
@@ -1,69 +1,27 @@
 ﻿using System;
-using System.Net;
-using System.Threading.Tasks;
-using System.IO;
-using System.Text;
 using System.Threading;
 
 namespace MainServer
 {
     class Program
     {
+
+        public static void test() {
+            Console.WriteLine("hello");
+        }
         //Negative feedback loop
 
         static void Main(string[] args)
         {
-            StartServer();
+            Thread webserverThread = new Thread(new ThreadStart(Webserver.StartServer));
+            Thread testThread = new Thread(new ThreadStart(test));
+
+            webserverThread.Start();
+            testThread.Start();
+
+            webserverThread.Join();
+            testThread.Join();
             Console.Read();
-        }
-
-        static HttpListener listener = new HttpListener();
-
-        static void StartServer()
-        {
-            listener.Prefixes.Add("http://localhost:8080/");
-            listener.Start();
-            listener.BeginGetContext(OnConnection, null);
-        }
-
-        static void OnConnection(IAsyncResult result)
-        {
-            listener.BeginGetContext(OnConnection, null);
-            new Task(() => ProcessRequest(result)).Start();
-        }
-
-        static void ProcessRequest(IAsyncResult result)
-        {
-            HttpListenerContext context = listener.EndGetContext(result);
-            Console.WriteLine(context.Request.Url.AbsolutePath);
-            HttpListenerResponse response = context.Response;
-
-            switch (context.Request.Url.AbsolutePath)
-            {
-                case "/":
-                    IndexPage(response);
-                    break;
-                default:
-                    response.StatusCode = 404;
-                    Stream output = response.OutputStream;
-                    output.Close();
-                    break;
-            }
-        }
-
-        static void IndexPage(HttpListenerResponse response)
-        {
-            string responseString = $"<HTML><BODY>Hello world! {DateTime.Now.ToLongTimeString()}</BODY></HTML>";
-            SimpleTextResponse(response, responseString);
-        }
-
-        static void SimpleTextResponse(HttpListenerResponse response, string responseString)
-        {
-            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-            response.ContentLength64 = buffer.Length;
-            Stream output = response.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            output.Close();
         }
     }
 }
---
+++ MainServer/Webserver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace MainServer {
+    class Webserver {
+        static HttpListener listener = new HttpListener();
+
+        public static void StartServer() {
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            listener.BeginGetContext(OnConnection, null);
+        }
+
+        static void OnConnection(IAsyncResult result) {
+            listener.BeginGetContext(OnConnection, null);
+            new Task(() => ProcessRequest(result)).Start();
+        }
+
+        static void ProcessRequest(IAsyncResult result) {
+            HttpListenerContext context = listener.EndGetContext(result);
+            Console.WriteLine(context.Request.Url.AbsolutePath);
+            HttpListenerResponse response = context.Response;
+
+            switch (context.Request.Url.AbsolutePath) {
+                case "/":
+                    IndexPage(response);
+                    break;
+                default:
+                    response.StatusCode = 404;
+                    Stream output = response.OutputStream;
+                    output.Close();
+                    break;
+            }
+        }
+
+        static void IndexPage(HttpListenerResponse response) {
+            string responseString = $"<HTML><BODY>Hello world! {DateTime.Now.ToLongTimeString()}</BODY></HTML>";
+            SimpleTextResponse(response, responseString);
+        }
+
+        static void SimpleTextResponse(HttpListenerResponse response, string responseString) {
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+        }
+    }
+}
