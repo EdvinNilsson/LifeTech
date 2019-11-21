5fc214a844f0ba33998b5b61a65d37188747ed19
Edvin Nilsson <edvinn.nilsson@gmail.com>
2019-11-21 18:08:35 +0100

    Add math library with map function

---
+++ RaspberryPi/MyMath.cs
@@ -0,0 +1,8 @@
+﻿namespace RaspberryPi
+{
+    public static class MyMath
+    {
+        public static float Map(this short value, short fromLow, short fromHigh, float toLow, float toHigh) => (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+        public static double Map(this short value, short fromLow, short fromHigh, double toLow, double toHigh) => (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+    }
+}
