fc786f446ce3cbd1aa33dafe6995d1f3e3398056
RedE01 <e.sternvik@gmail.com>
2019-12-10 10:24:10 +0100

    Add Sensordata class

---
+++ RaspberryPi/SensorData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaspberryPi {
+    class SensorData {
+        DateTime timestamp;
+        Sensor[] sensors;
+    }
+}
