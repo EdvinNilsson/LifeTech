79754dcdf51b0e75e8cae458af19307105f0b91d
Edvin Nilsson <edvinn.nilsson@gmail.com>
2020-02-04 14:04:59 +0100

    Add GetValueCreateNew

--- SharedStuff/MyMath.cs
+++ SharedStuff/MyMath.cs
@@ -1,8 +1,19 @@
-﻿namespace SharedStuff
+﻿using System.Collections.Generic;
+
+namespace SharedStuff
 {
     public static class MyMath
     {
         public static float Map(this short value, short fromLow, short fromHigh, float toLow, float toHigh) => (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
         public static double Map(this short value, short fromLow, short fromHigh, double toLow, double toHigh) => (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+
+        public static TValue GetValueCreateNew<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key) where TValue : new() {
+            try {
+                return dictionary[key];
+            } catch (KeyNotFoundException) {
+                dictionary.Add(key, new TValue());
+                return dictionary[key];
+            }
+        }
     }
 }
