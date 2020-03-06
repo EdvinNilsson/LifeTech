a65c7948c714e11bf85713d0c231d3009b2d2434
Edvin Nilsson <edvinn.nilsson@gmail.com>
2020-03-06 19:07:52 +0100

    Socket fix

--- RaspberryPi/SocketClient.cs
+++ RaspberryPi/SocketClient.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
+using System.Threading;
 using SharedStuff;
 
 namespace RaspberryPi {
@@ -39,6 +39,7 @@ namespace RaspberryPi {
             sender.Close();
         }
 
+        bool isSending = false;
         byte[] buffer = new byte[4];
         public bool SendMessage(byte[] message, MessageType messageType) {
             uint length = (uint) message.Length;
@@ -46,11 +47,17 @@ namespace RaspberryPi {
             buffer[1] = (byte) (length >> 8);
             buffer[2] = (byte) (length >> 16);
             buffer[3] = (byte) messageType;
+            while (isSending) {
+                Thread.Sleep(1);
+            }
             try {
+                isSending = true;
                 sender.Send(buffer);
                 sender.Send(message);
+                isSending = false;
             }
             catch {
+                isSending = false;
                 return false;
             }
             return true;
